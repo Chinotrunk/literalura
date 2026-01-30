b089ef6271e7d821749226b672137f26257f9ab0
chinotrunk <gerson2050@hotmail.com>
2026-01-29 19:35:27 -0500

    feat: add CommandLineRunner to start console menu

--- src/main/java/com/davidlima/literalura/LiteraluraApplication.java
+++ src/main/java/com/davidlima/literalura/LiteraluraApplication.java
@@ -1,13 +1,26 @@
 package com.davidlima.literalura;
 
+import com.davidlima.literalura.principal.Principal;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LiteraluraApplication {
+public class LiteraluraApplication implements CommandLineRunner {
+
+  private final Principal principal;
+
+  public LiteraluraApplication(Principal principal) {
+    this.principal = principal;
+  }
+
+  @Override
+  public void run(String... args) {
+    principal.muestraElMenu();
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(LiteraluraApplication.class, args);
   }
-
 }
+
